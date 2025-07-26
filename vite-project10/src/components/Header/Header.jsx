4fc914135abf965765949228df43f8bba5760a47
Gtavarez <giselltavarez@gmail.com>
2025-07-25 21:22:20 -0400

    Full project structure

--- vite-project10/src/components/Header/Header.jsx
+++ vite-project10/src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import "./header.css";
 import avatar from "../../assets/avatar.png";
 import logo from "../../assets/logo.svg";
-function Header() {
+function Header({ handleAddClick }) {
   return (
     <>
       <h1 className="header">
         <img className="header__logo" src={logo} />
         <p className="header__date-and-location">DATE, LOCATION</p>
-        <button className="header__add-clothes-btn">+ Add Clothes</button>
+        <button
+          onClick={handleAddClick}
+          type="button"
+          className="header__add-clothes-btn"
+        >
+          + Add Clothes
+        </button>
         <div className="header__user-container">
           <p className="header__username">Name</p>
           <img src={avatar} alt="" className="header__avatar" />
