a93010304e2b42c8fa8a8abf2bddbc1c2f630afc
Gtavarez <giselltavarez@gmail.com>
2025-08-06 15:12:37 -0400

    copy inner contents of vite-project10 folder to outside

---
+++ src/components/App/App.jsx
@@ -0,0 +1,103 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "../Header/Header.jsx";
+import Main from "../Main/Main.jsx";
+import ModalWithForm from "../ModalWithForm/ModalWithform.jsx";
+import ItemModal from "../ItemModal/ItemModal.jsx";
+import Footer from "../Footer/Footer.jsx";
+import { getWeather, filterWeatherData } from "../../utils/weatherApi.js";
+import { coordinates, APIkey } from "../../utils/constants.js";
+function App() {
+  const [weatherData, setWeatherData] = useState({
+    type: "",
+    temp: { F: 999 },
+    city: "",
+  });
+  const [activeModal, setActiveModal] = useState("");
+  const [selectedCard, setSelectedCard] = useState({});
+  const handleAddClick = () => {
+    setActiveModal("add-garment");
+  };
+  const closeActiveModal = () => {
+    setActiveModal("");
+  };
+  const handleCardClick = (item) => {
+    setActiveModal("preview");
+    setSelectedCard(item);
+  };
+  useEffect(() => {
+    getWeather(coordinates, APIkey)
+      .then((data) => {
+        const filteredData = filterWeatherData(data);
+        setWeatherData(filteredData);
+      })
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header handleAddClick={handleAddClick} weatherData={weatherData} />
+        <Main
+          weatherData={weatherData}
+          setActiveModal={setActiveModal}
+          handleCardClick={handleCardClick}
+        />
+      </div>
+      <ModalWithForm
+        buttonText="Add garment"
+        title="New garment"
+        activeModal={activeModal}
+        onClose={closeActiveModal}
+      >
+        <label htmlFor="name" className="modal__label">
+          Name{" "}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="Name"
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image{" "}
+          <input
+            type="url"
+            className="modal__input"
+            id="imageUrl"
+            placeholder="ImageUrl"
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input id="hot" type="radio" className="modal__radio-input" />
+            hot
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input id="warm" type="radio" className="modal__radio-input" />
+            warm
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input id="cold" type="radio" className="modal__radio-input" />
+            cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
+      <ItemModal
+        activeModal={activeModal}
+        card={selectedCard}
+        onClose={closeActiveModal}
+      />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
